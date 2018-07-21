ec6e6946bc012a8b45e687a7e104ccd6c0edb1f0
oscar0812 <oscar0812torres@gmail.com>
2018-07-21 13:21:19 -0500

    routes for home and blogs done

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@ require '../vendor/autoload.php';
 
 // adding an external config file
 require '../config.php';
-//require '../data/generated-conf/config.php';
+require '../data/generated-conf/config.php';
 
 $app = new \Slim\App(["settings" => $config]);
 
@@ -20,11 +20,32 @@ $container['notFoundHandler'] = function ($c) {
 };
 
 $app->get('/', function ($request, $response, $args) {
-    return $this->view->render($response, "home.php", []);
+    return $this->view->render(
+        $response,
+        'home.php',
+        ['router'=>$this->router, 'posts'=>\PostQuery::create()->find(), 'categories'=>\CategoryQuery::create()->find()]
+    );
 })->setName('home');
 
-$app->get('/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, $args['name'], []);
-});
+$app->get('/post-{hyperlink}', function ($request, $response, $args) {
+    $post = \PostQuery::create()->findOneByHyperlink($args['hyperlink']);
+
+    if ($post == null) {
+        // invalid post, throw 404
+        throw new \Slim\Exception\NotFoundException($request, $response);
+    }
+    return $this->view->render(
+        $response,
+        'blog-post.php',
+        ['router'=>$this->router, 'post'=>$post]
+    );
+})->setName('blog-post');
+
+$router = $app->getContainer()->router;
+
+// to avoid errors when fetching files from the html folder,
+// routes will try to look at html/index.php/ which is a valid url,
+// but not a valid directory
+define('$home', replaceLast('index.php/', '', $router->pathFor('home')));
 
 $app->run();
