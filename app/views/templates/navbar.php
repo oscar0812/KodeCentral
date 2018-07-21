94cb6bc36d9edfe371249d409038a57ce8dcafaf
oscar0812 <oscar0812torres@gmail.com>
2018-07-21 12:15:12 -0500

    templates for views

---
+++ app/views/templates/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-md navbar-static ms-navbar
+ms-navbar-primary navbar-mode stay-on-top-navbar">
+  <div class="container container-full">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">
+        <!-- <img src="assets/img/demo/logo-navbar.png" alt=""> -->
+        <span class="ms-logo ms-logo-sm">KC</span>
+        <span class="ms-title">Kode
+          <strong>Central</strong>
+        </span>
+      </a>
+    </div>
+  </div>
+
+  <!-- container -->
+</nav>
