440e30fbea004dc3bc64a8826ed798708095728e
oscar0812 <oscar0812torres@gmail.com>
2018-07-22 00:02:36 -0500

    make sure db knows username and email must be unique

--- data/models/User.php
+++ data/models/User.php
@@ -12,7 +12,37 @@ use Base\User as BaseUser;
  * long as it does not already exist in the output directory.
  *
  */
+
+use Propel\Runtime\Connection\ConnectionInterface;
+use Propel\Runtime\Map\TableMap;
+
 class User extends BaseUser
 {
+    // hash the password before saving
+    public function save(ConnectionInterface $con = null)
+    {
+        // hash password
+        $password = PHPassLib\Hash\BCrypt::hash(parent::getPassword());
+        // store the Hash
+        parent::setPassword($password);
 
+        parent::save();
+    }
+
+    // returns true if $password => hashed($password)
+    public function login($password)
+    {
+        return PHPassLib\Hash\BCrypt::verify($password, $this->getPassword());
+    }
+
+    // for validation purposes, validation seems to fail with empty strings,
+    // so if empty string, replace with null, and schema.xml validate
+    // knows how to check for nulls
+    public function setByName($name, $str, $type = TableMap::TYPE_PHPNAME)
+    {
+        if ($str == '') {
+            $str = null;
+        }
+        return parent::setByName($name, $str, $type);
+    }
 }
