65e133df52a6a7202bda2338d8af9a8c5c6c9e17
oscar0812 <oscar0812torres@gmail.com>
2018-07-23 18:28:03 -0500

    test route with view

--- html/index.php
+++ html/index.php
@@ -24,37 +24,14 @@ $container['notFoundHandler'] = function ($c) {
     };
 };
 
-$app->get('/', function ($request, $response, $args) {
-    return $this->view->render(
-        $response, 'home.php',
-        ['router'=>$this->router,
-        'posts'=>\PostQuery::create()->find(),
-        'all_categories'=>\CategoryQuery::create()->find()]
-    );
-})->setName('home');
-
-$app->get('/post-{hyperlink}', function ($request, $response, $args) {
-    $post = \PostQuery::create()->findOneByHyperlink($args['hyperlink']);
-
-    if ($post == null) {
-        // invalid post, throw 404
-        throw new \Slim\Exception\NotFoundException($request, $response);
-    }
-    return $this->view->render(
-        $response,
-        'blog-post.php',
-        ['router'=>$this->router, 'post'=>$post]
-    );
-})->setName('blog-post');
-
 $app->get('/test', function ($request, $response, $args) {
-    echo "test";
-    $user = new \User();
-    $user->setJoinDate(getCurrentDate());
-    print_r($user->toArray());
-    return null;
-})->setName('home');
+  return $this->view->render(
+      $response,
+      'test.php',
+      ['router'=>$this->router]);
+});
 
+App\Controllers\AllController::setUpRouting($app);
 App\Controllers\LoggedInController::setUpRouting($app);
 App\Controllers\LoggedOutController::setUpRouting($app);
 
