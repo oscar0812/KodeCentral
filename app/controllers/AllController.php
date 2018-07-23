bb1e56fdee665d01665b1017c73ca7ff895655d9
oscar0812 <oscar0812torres@gmail.com>
2018-07-23 18:21:19 -0500

    controller for routes that can be seen when signed in or not

---
+++ app/controllers/AllController.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Controllers;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Exception\NotFoundException;
+
+// All the routes here can be seen if logged in or out, doesnt matter
+class AllController
+{
+    public function home($app)
+    {
+        $app->get('/', function ($request, $response, $args) {
+            return $this->view->render(
+                $response,
+                'home.php',
+                ['router'=>$this->router,
+                'posts'=>\PostQuery::create()->find(),
+                'all_categories'=>\CategoryQuery::create()->find()]
+            );
+        })->setName('home');
+    }
+
+    public function post($app)
+    {
+        $app->get('/post/{hyperlink}', function ($request, $response, $args) {
+            $post = \PostQuery::create()->findOneByHyperlink($args['hyperlink']);
+
+            if ($post == null) {
+                // invalid post, throw 404
+                throw new \Slim\Exception\NotFoundException($request, $response);
+            }
+            return $this->view->render(
+                $response,
+                'blog-post.php',
+                ['router'=>$this->router, 'post'=>$post]
+            );
+        })->setName('blog-post');
+    }
+
+    public function profile($app)
+    {
+        // when visiting someones profile
+        $app->get('/profile/{username}', function ($request, $response, $args) {
+            $user = \UserQuery::create()->findOneByUsername($args['username']);
+            
+            $current = \User::current();
+            $visiting = true;
+            if ($current != null && $current->getId() == $user->getId()) {
+                $visiting = false;
+            }
+            return $this->view->render(
+              $response, 'page-profile.php',
+              ['router'=>$this->router, 'user'=>$user, 'visiting'=>$visiting]
+          );
+        })->setName('user-profile');
+    }
+
+    public static function setUpRouting($app)
+    {
+        $controller = new AllController();
+        $controller->home($app);
+        $controller->post($app);
+        $controller->profile($app);
+    }
+}
