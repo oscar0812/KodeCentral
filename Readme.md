47297f954e38820dbca5903430a5ad1ee9da47c8
oscar0812 <oscar0812torres@gmail.com>
2018-08-02 13:20:15 -0500

    always keep filter visible

--- Readme.md
+++ Readme.md
@@ -1,6 +1,4 @@
 TODO:
-  - add validation to models (did most of them)
-  - Add an option to hide/show the comments (done)
   - Add a route to show all libraries
   - Add a way to create a library (or change the library of post)
     when creating/editing a post
--- app/views/home.php
+++ app/views/home.php
@@ -44,7 +44,7 @@
       </div>
       <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-3 d-none d-lg-block">
+          <div class="col-md-3">
             <div class="card card-primary">
               <div class="card-header">
                 <h3 class="card-title">
