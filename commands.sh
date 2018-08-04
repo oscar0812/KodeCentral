b4e5f8705e3a614fc9645b0b6f84f6642cf9fb5e
oscar0812 <oscar0812torres@gmail.com>
2018-08-03 21:18:12 -0500

    commands that must be ran on the server

---
+++ commands.sh
@@ -0,0 +1,6 @@
+# commands that have to be ran every time the directory is changed to
+# update priviledges
+chmod 777 html/assets/img/pfp/
+chmod 755 html/assets/js/*
+chmod 755 html/assets/css/*
+composer dump-autoload -o
