e3900a4fc969696b10421a9d518342e68af9a110
oscar0812 <oscar0812torres@gmail.com>
2018-08-15 11:10:17 -0500

    home text changed

--- app/views/home.php
+++ app/views/home.php
@@ -33,8 +33,8 @@
         <div class="container">
           <div class="text-center">
             <h1 class="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Home</h1>
-            <p class="lead lead-lg color-white text-center center-block mt-2 mb-4 mw-800 text-uppercase fw-300 animated fadeInUp animation-delay-7">Discover our projects and the
-              <span class="color-warning">rigorous process</span> of creation. Our principles are creativity, design, experience and knowledge.</p>
+            <p class="lead lead-lg color-white text-center center-block mt-2 mb-4 mw-800 text-uppercase fw-300 animated fadeInUp animation-delay-7">
+            Become a better programmer and contribute. Topics range from <span class="color-warning">dynamic programming</span> to almost any other topic in computer science.</p>
               <a href="<?=$router->pathFor('contact-us')?>" class="btn btn-raised btn-info animated fadeInUp animation-delay-10">
                 <i class="zmdi zmdi-email"></i> Contact us</a>
               <a href="<?=$router->pathFor('faq')?>" class="btn btn-raised btn-warning animated fadeInUp animation-delay-10">
