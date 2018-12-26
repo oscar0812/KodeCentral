543bd46d63bcdc4e2fd8b3d6e3255f290e5441d4
oscar0812 <oscar0812torres@gmail.com>
2018-12-26 14:44:08 -0600

    send ip with email (stop spammers)

--- config.php
+++ config.php
@@ -83,3 +83,17 @@ function getCurrentTime()
 {
     return getCurrentDateTime()->getTimestamp();
 }
+
+function getUserIP()
+{
+    if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+            $addr = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim($addr[0]);
+        } else {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+    } else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
