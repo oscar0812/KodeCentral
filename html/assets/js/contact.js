2ecffe49c24ac1a605cea1691632219a51ffb07f
oscar0812 <oscar0812torres@gmail.com>
2019-02-25 13:30:48 -0600

    made google reCAPTCHA v2 be responsive on phones

--- html/assets/js/contact.js
+++ html/assets/js/contact.js
@@ -1,4 +1,13 @@
 $(function() {
+  var width = $('.g-recaptcha').parent().width();
+  if (width < 302) {
+    var scale = width / 302;
+    $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+    $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+    $('.g-recaptcha').css('transform-origin', '0 0');
+    $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+  }
+  
   $('#contact-form').on('submit', function(e) {
     Snackbar.show({
       actionTextColor: '#ffff00',
